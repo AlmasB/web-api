3a846292f570d104513ad5629f430d950ed55e4b
Almas Baim <almaslvl@gmail.com>
2023-06-16 15:06:40 +0100

    cont

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ socket.addEventListener('message', function (event) {
 
 // set up speech recog
 const recognition = new SpeechRecognition();
-recognition.continuous = false;
+recognition.continuous = true;
 recognition.lang = 'en-GB';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
